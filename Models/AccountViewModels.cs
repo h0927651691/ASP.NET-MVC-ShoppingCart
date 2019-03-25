e15a33665c8f28a28c8e2f6e247e621f2d51071b
h0927651691 <h0927651691@gmail.com>
2019-03-25 23:00:55 +0800

    加入會員暱稱功能

--- Models/AccountViewModels.cs
+++ Models/AccountViewModels.cs
@@ -64,6 +64,11 @@ namespace ShoppingCarts.Models
 
     public class RegisterViewModel
     {
+        [Required]   //必要欄位
+        [StringLength(256,ErrorMessage = "{0}的長度至少必須為{2}個字元。",MinimumLength = 1) ] //字元長度1~256
+        [Display(Name = "暱稱")] //欄位顯示文字
+        public string UserName { get; set; }
+
         [Required]
         [EmailAddress]
         [Display(Name = "電子郵件")]
