886e1d200f6bf559d638134aa523788f1f8f9b0f
h0927651691 <h0927651691@gmail.com>
2019-03-26 23:18:37 +0800

    20190326新增使用者清單功能

---
+++ Models/ManageUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingCarts.Models
+{
+    public class ManageUser
+    {
+        [Required] //必要欄位
+        public string Id { get; set; }
+
+        [Required] //必要欄位
+        [StringLength(256,ErrorMessage = "{0}的長度必須為{2}個字元。",MinimumLength =1)] //字元長度1~256
+        [Display(Name ="暱稱")] //顯示文字
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "電子郵件")]
+        public string Email { get; set; }
+    }
+}
