39847ab58145274e1715a5c75598d4486ac517bd
h0927651691 <h0927651691@gmail.com>
2019-03-27 22:51:21 +0800

    20190327 測試購物車功能

---
+++ Controllers/TestController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShoppingCarts.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetCart()
+        {
+            //取得目前購物車
+            var cart = Models.Cart.Operation.GetCurrentCart();
+
+            //如果購物車內無任何商品，則新增一筆
+            if( cart.cartItems.Count == 0)
+            {
+                cart.cartItems.Add(
+                    new Models.Cart.CartItem()
+                    {
+                        Id = 1,
+                        Name = "測試",
+                        Quantity = 1,
+                        Price = 100m
+                    });
+            }
+            else //若有商品，則將第一筆商品數量+1
+            {
+                cart.cartItems.First().Quantity += 1;
+            }
+            //回傳目前購物車中的商品總價
+            return Content(string.Format("目前購物車共:{0}元", cart.TotalAmout));
+        }
+    }
+}
---
+++ Models/Cart/Operation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace ShoppingCarts.Models.Cart
+{
+    public static class Operation
+    {
+        [WebMethod(EnableSession = true)] //啟用Session
+        public static Models.Cart.Cart GetCurrentCart() //取得目前Session中的Cart物件
+        {
+            if (System.Web.HttpContext.Current != null) //確認System.Web.HttpContext.Current是否為空
+            {
+
+                if (System.Web.HttpContext.Current.Session["Cart"] == null)
+                {
+                    var order = new Cart();
+                    System.Web.HttpContext.Current.Session["Cart"] = order;
+                }
+                //回傳Session["Cart"]
+                return (Cart)System.Web.HttpContext.Current.Session["Cart"];
+            }
+            else
+            {
+                throw new InvalidCastException("System.Web.HttpVontext.Current為空，請檢查");
+            }
+        }
+    }
+}
